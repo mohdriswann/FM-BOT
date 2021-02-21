6edb5b7fedd1382b2fb57258af2022b4eb154316
ALEN TL <67356843+ALENTL@users.noreply.github.com>
2021-02-22 00:55:52 +0530

    Update math.py

--- tg_bot/modules/math.py
+++ tg_bot/modules/math.py
@@ -1,8 +1,8 @@
 import math
 
 import pynewtonmath as newton
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from tg_bot import dispatcher
+from tg_bot.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
